93608cecfef05f07802213e32ed07e20109d14d7
Dun <dung8ahxh@gmail.com>
2024-09-13 23:00:44 +0700

    Header Part1

--- src/Components/Layout/components/Header/index.tsx
+++ src/Components/Layout/components/Header/index.tsx
@@ -1,5 +1,8 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '../../../../assets';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -8,9 +11,31 @@ const Header = () => {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 {/* Logo */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt='tiktok'></img>
+                </div>
+
+                <div className={cx('search')}>
+                    <input type='text' placeholder='Search accounts and videos' spellCheck={false}></input>
+
+                    <button className={cx('clear-btn')}>
+                        {/* icon close */}
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+
+                    {/* Loading */}
+                    <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+
+                <div className={cx('actions')}>
+
+                </div>
             </div>
-        </header>
+        </header >
     )
 }
 
-export default Header;
+export default Header; 
---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+// index.tsx or another ts file
+import logo from "./tiktok-banner-black-3.svg";
+
+const images = {
+  logo,
+};
+
+export default images;
